14a36868ccc7e6be6f0f4004bcd6298f1987e7e1
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-04-16 17:53:53 +0200

    gitpush

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const apiBase = "https://www.devbergen.no.mysql";
+const wooCommerceBase = "/wp-json/wc/store";
+const productBase = "/products";
+
+const pagesBase ="/wp-json/wp/v2/pages";
+
+const fullPagesURL = apiBase +pagesBase;
+const fullProductURL = apiBase +wooCommerceBase + productBase;
