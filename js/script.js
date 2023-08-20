d7f1919e2ddc97045923b7420a581508f2a7901f
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-08-20 19:49:26 +0200

    git push

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const apiBase = "https://www.devbergen.no.mysql";
+const apiBase = "http://olga-rainy-days-cms.local";
 const wooCommerceBase = "/wp-json/wc/store";
 const productBase = "/products";
 
-const pagesBase ="/wp-json/wp/v2/pages";
+const pagesBase = "/wp-json/wp/v2/pages";
 
-const fullPagesURL = apiBase +pagesBase;
-const fullProductURL = apiBase +wooCommerceBase + productBase;
+const fullPagesURL = apiBase + pagesBase;
+const fullProductURL = apiBase + wooCommerceBase + productBase;
