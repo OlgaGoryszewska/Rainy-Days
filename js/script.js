bbc561dd63d20a9da1150ff458c38589bb7f1e72
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-08-20 22:27:44 +0200

    git push

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ const pagesBase = "/wp-json/wp/v2/pages";
 
 const fullPagesURL = apiBase + pagesBase;
 const fullProductURL = apiBase + wooCommerceBase + productBase;
+const fullProductURLExample = "http://olga-rainy-days-cms.local/wp-json/wc/store/products";
+
+async function getProducts() {
+    const response = await fetch(fullProductURL);
+    const body = await response.json();
+    console.log(body);
+};
+
+getProducts();
