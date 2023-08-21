05381d5cea6d973a87bc34598913c9fccdaa40ed
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-08-21 14:45:34 +0200

    git push

--- js/script.js
+++ js/script.js
@@ -6,19 +6,12 @@ const pagesBase = "/wp-json/wp/v2/pages";
 
 const fullPagesURL = apiBase + pagesBase;
 const fullProductURL = apiBase + wooCommerceBase + productBase;
-const fullProductURLExample = "http://olga-rainy-days-cms.local/wp-json/wc/store/products";
-
-
-
-
 
 async function getProducts() {
     const response = await fetch(fullProductURL);
-    const body = await response.json();
-    console.log(response)
-
+    const products = await response.json();
+    return products;
 }
-
 
 function createProductHTML(product) {
     const container = document.querySelector(".container");
@@ -26,35 +19,31 @@ function createProductHTML(product) {
     productContainer.classList.add("product");
     productContainer.id = product.id;
 
-    const title = document.createElement("h2")
+    const title = document.createElement("h2");
     title.innerText = product.name;
-    productContainer.append(title)
+    productContainer.appendChild(title);
 
-    console.log(product.images);
-
-
-    for (let i = 0; i < product.images; i++) {
+    for (let i = 0; i < product.images.length; i++) {
         const imgData = product.images[i];
         const img = document.createElement("img");
         img.src = imgData.src;
         img.alt = imgData.alt;
-        productContainer.append(img)
+        productContainer.appendChild(img);
     }
 
-
-
+    container.appendChild(productContainer); // Append the product container to the container in the DOM
 }
 
-
-
-function createProductsHTML(product) {
-    for (let i = 0; i < product.lenght; i++) {
+async function createProductsHTML() {
+    const products = await getProducts();
+    for (let i = 0; i < products.length; i++) {
         const product = products[i];
-        createProductHTML(product)
+        createProductHTML(product);
     }
 }
 
 async function main() {
-    const products = await getProducts()
-    createProductsHTML
+    await createProductsHTML();
 }
+
+main();
